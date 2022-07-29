e9ebfd73d64d376bd3de3b85eba12efb2d7faa92
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2022-07-29 06:02:17 +0200

    Add script to delete files not needed for us. This will shrink the tarball for crab submission, making it faster to be sent.

---
+++ cleanup_forCrab.sh
@@ -0,0 +1,10 @@
+rm -rf data/btagSF/*.csv
+
+rm -rf data/jme/Autumn18*
+rm -rf data/jme/Fall17*
+rm -rf data/jme/Summer16*
+rm -rf data/jme/Summer19UL17_Run*_V5_DATA.tgz
+rm -rf data/jme/Summer19UL18_Run*_V5_DATA.tgz
+
+rm -rf python/postprocessing/data/roccor*
+rm -rf python/postprocessing/data/tau/*.root
